2aebe8dc449b12f939ae7adc46c9df0fb920d893
Diogo Henrique <diogjp10@gmail.com>
2026-07-13 14:28:54 +0000

    Add bottom navigation view with 4 tabs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:vita_folder_mobile/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Bottom navigation switches tabs', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Home Content'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Search'));
     await tester.pump();
+    expect(find.text('Search Content'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('Favorites'));
+    await tester.pump();
+    expect(find.text('Favorites Content'), findsOneWidget);
+
+    await tester.tap(find.text('Profile'));
+    await tester.pump();
+    expect(find.text('Profile Content'), findsOneWidget);
+
+    await tester.tap(find.text('Home'));
+    await tester.pump();
+    expect(find.text('Home Content'), findsOneWidget);
   });
 }
