00e526cabec907dae21d26a1151855bf7cbc8de1
Diogo Henrique <diogjp10@gmail.com>
2026-07-14 14:02:07 +0000

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:vita_folder_mobile/features/reminders/presentation/screens/reminders_view.dart';
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vita Folder Mobile',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const RemindersView(),
-    );
-  }
-}
+// The app entrypoint is defined in main.dart.
+export 'main.dart' show MyApp, MainView;
